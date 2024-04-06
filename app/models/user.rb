80c3ab30cf20f543884ab270c11212dab79d87be
ARTurleite6 <arturleite164@gmail.com>
2024-04-06 06:03:29 +0100

    annotate and some validations on the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  first_name :string           not null
+#  last_name  :string           not null
+#  username   :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
+  validates :first_name, :last_name, :username, presence: true
+
+  has_many :friends, class_name: 'Friendship'
+  has_many :reviews, dependent: :destroy
+  has_many :wishlists, dependent: :destroy
 end
--- app/models/user_friend.rb
+++ app/models/user_friend.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: user_friends
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  friend_id  :bigint           not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_user_friends_on_friend_id  (friend_id)
+#  index_user_friends_on_user_id    (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (friend_id => users.id)
+#  fk_rails_...  (user_id => users.id)
+#
 class UserFriend < ApplicationRecord
   belongs_to :user
   belongs_to :friend
